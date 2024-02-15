b6145ab3cd19d5cf5db0a764d3a287b1662f34a5
wtrewa <psaurabh574@gmail.com>
2024-02-15 22:44:26 +0530

    lastupdate

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const connection = require('./connetion')
 const cors = require('cors');
 const { json } = require('body-parser');
 const playerRouter = require('./route/playerRoute');
+const swaggerJSDoc = require('swagger-jsdoc')
+const swaggerUiExpress = require('swagger-ui-express')
 
 const app = express()
 const csvParser = require('csv-parser');
 const { userRouter } = require('./route/userRoute');
 const auth = require('./middleware/auth');
+const swaggerJSDoc = require('swagger-jsdoc');
 
 app.use(cors())
 app.use(express.json())
@@ -19,6 +22,7 @@ app.get('/',(req,res)=>{
     res.send("hello world")
 })
 
+const spacs = swaggerJSDoc()
 app.listen(8080,()=>{
     connection()
     console.log('port is running on 8080')
